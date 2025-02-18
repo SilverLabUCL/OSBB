d959a3c86d99c5bc74fb4b4f978bccbeacf46431
pgleeson <p.gleeson@gmail.com>
2025-02-18 11:21:36 +0000

    More tweaks

--- run_nds.sh
+++ run_nds.sh
@@ -1,2 +1,5 @@
 # /usr/local/opt/ruby/bin/bundle exec jekyll serve  --incremental
-docker run --volume="$PWD:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll:3.8 jekyll serve --watch 
+#docker run --volume="$PWD:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll:4.0.0 jekyll serve --watch 
+
+export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
+bundle exec jekyll serve --watch
